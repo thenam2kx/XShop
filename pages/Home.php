d2bf3dd7b8151aad14ec1ae950582e4802e9bd55
Thenam2kx <thenam2kx@gmail.com>
2024-06-25 12:28:25 +0700

    upload header

--- pages/Home.php
+++ pages/Home.php
@@ -1,15 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Structure Sass</title>
+  <title>XShop</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="./css/style.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="../assets/bootstrap//css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
 </head>
-<body>
 
+<body>
+  <div class="container">
+    <header class="header">
+      <section class="header--logo">
+        <a href="">XShop</a>
+      </section>
+      <section class="header--nav">
+        <nav class="nav">
+          <a href="" class="nav__link active">Trang chu</a>
+          <a href="" class="nav__link">Gioi thieu</a>
+          <a href="" class="nav__link">Lien he</a>
+          <a href="" class="nav__link">Gop y</a>
+          <a href="" class="nav__link">Hoi dap</a>
+        </nav>
+      </section>
+      <section class="header--user">
+          <a href="" class="user">
+            <i class='bx bx-user-circle user__icon'></i>
+            <span class="user__text">Thenam2kx</span>
+          </a>
+      </section>
+    </header>
+
+    <main>
+      
+    </main>
+
+    <footer></footer>
+  </div>
+
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
